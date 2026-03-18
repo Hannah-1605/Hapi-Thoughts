52c88ec09db2f27d2809250be0e796acb5af3407
Krys <krysbesana@gmail.com>
2026-03-19 00:39:26 +0800

    db @ settings added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hapi_tots_db',
+        'USER': 'vet_admin', 
+        'PASSWORD': 'hapivet',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
